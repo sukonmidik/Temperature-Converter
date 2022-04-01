6f0fe47a63f0936bfe7f1f9f33c4e182c74bdc07
sukonmidik <95209407+sukonmidik@users.noreply.github.com>
2022-04-01 17:57:23 -0400

    Add files via upload

---
+++ Converter.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+namespace Temperature_Convertor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private static decimal temperature;
+        private bool ValidInput(decimal input)
+        {
+            var matches = Regex.Matches(tempinput.Text, "^-?[0-9]{1,4}([.,][0-9]{1,2})?$");
+            foreach (Match match in matches )
+            {
+                return true;
+            }
+            return false;
+        }
+        private decimal ConvertTemp(decimal input)
+        {
+            if (radioButton1.Checked)
+            {
+                // F > C
+                input = (input - 32) * 5 / 9;
+                MessageBox.Show(temperature.ToString() +"°F" + " = " + decimal.Truncate(input * 100) / 100 + "°C", "Calculation", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return input;
+            }
+            else if (radioButton2.Checked)
+            {
+                // C > F
+                input = (input * Convert.ToDecimal(1.8) + 32);
+                MessageBox.Show(temperature.ToString() + "°C" + " = " + decimal.Truncate(input * 100) / 100 + "°F", "Calculation", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return input;
+            }
+            return 0;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                temperature = Convert.ToDecimal(tempinput.Text);
+                if (ValidInput(temperature))
+                {
+                    ConvertTemp(temperature);
+                }
+                else
+                {
+                    MessageBox.Show("Please Enter A Value Between -9999.99 To 9999.99", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Select A Conversion Method.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
